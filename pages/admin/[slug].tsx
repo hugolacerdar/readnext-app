946a32eeeeff31fe89356c922be92a09ec3ca249
Hugo <hugolacerdar@gmail.com>
2021-12-14 08:35:32 -0300

    Add base pages

---
+++ pages/admin/[slug].tsx
@@ -0,0 +1,3 @@
+export default function AdminPostEditPage({}) {
+  return <main></main>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import type { NextPage } from "next";
+import Link from "next/link";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Loader from "../components/Loader";
+import toast from "react-hot-toast";
 
-const Home: NextPage = () => {
-  return <div></div>;
-};
-
-export default Home;
+export default function Home() {
+  return <div>Home</div>;
+}
