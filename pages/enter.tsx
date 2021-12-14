74e90d9ce5ff1796576251b29b66cbce17a855e3
Hugo <hugolacerdar@gmail.com>
2021-12-14 13:01:16 -0300

    Add sign in button

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,3 +1,38 @@
+import { auth, googleAuthProvider } from "../lib/firebase";
+
 export default function EnterPage({}) {
-  return <main>Signup</main>;
+  const user = null;
+  const username = null;
+
+  return (
+    <main>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
+    </main>
+  );
+}
+
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    // TODO - apply error handler
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google-logo.png"} alt="google logo" /> Sign in with Google
+    </button>
+  );
+}
+function SignOutButton() {
+  return <></>;
+}
+function UsernameForm() {
+  return <></>;
 }
