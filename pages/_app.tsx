0106d2f136c44ad464508edf09091ab006ab812c
Hugo <hugolacerdar@gmail.com>
2021-12-14 13:16:21 -0300

    Add user context provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 import { Toaster } from "react-hot-toast";
+import { UserContext } from "../lib/userContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <UserContext.Provider value={{ user: {}, username: "hugo" }}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </UserContext.Provider>
   );
 }
 
