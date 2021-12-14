f4b3f047b558173e7c6cf53334db03032646cd25
Hugo <hugolacerdar@gmail.com>
2021-12-14 18:46:56 -0300

    Add helper functions

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -22,3 +22,20 @@ export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
+
+export async function getUserWithUsername(username: string) {
+  const usersRef = firestore.collection("users");
+  const query = usersRef.where("username", "==", username).limit(1);
+  const userDoc = (await query.get()).docs[0];
+  return userDoc;
+}
+
+export function postToJSON(doc: any) {
+  const data = doc.data();
+
+  return {
+    ...data,
+    createdAt: data.createdAt.toMillis(),
+    updatedAt: data.createdAt.toMillis(),
+  };
+}
