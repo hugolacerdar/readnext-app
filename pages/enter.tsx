ef707d114ee54510dbe3d74c2c318086bbd344b5
Hugo <hugolacerdar@gmail.com>
2021-12-14 15:07:33 -0300

    Setup username form and submition

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,6 +1,13 @@
-import { useContext } from "react";
+import React, {
+  FormEvent,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { UserContext } from "../lib/userContext";
-import { auth, googleAuthProvider } from "../lib/firebase";
+import { auth, firestore, googleAuthProvider } from "../lib/firebase";
+import debounce from "lodash.debounce";
 
 export default function EnterPage({}) {
   const { user, username } = useContext(UserContext);
@@ -27,7 +34,8 @@ function SignInButton() {
   };
   return (
     <button className="btn-google" onClick={signInWithGoogle}>
-      <img src={"/google-logo.png"} alt="google logo" /> Sign in with Google
+      <img src="/google-logo.png" alt="google logo" height="30" width="30" />{" "}
+      Sign in with Google
     </button>
   );
 }
@@ -39,5 +47,118 @@ function SignOutButton() {
   );
 }
 function UsernameForm() {
-  return <></>;
+  const [formValue, setFormValue] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { user, username } = useContext(UserContext);
+
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const val = e.target.value.toLowerCase();
+    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+    if (val.length < 3) {
+      setFormValue(val);
+      setLoading(false);
+      setIsValid(false);
+    }
+
+    if (re.test(val)) {
+      setFormValue(val);
+      setLoading(true);
+      setIsValid(false);
+    }
+  }
+
+  const checkUsername = useCallback(
+    debounce(async (username: string) => {
+      if (username.length >= 3) {
+        const ref = firestore.doc(`usernames/${username}`);
+        const { exists } = await ref.get();
+        console.log("Firestore read executed");
+        setIsValid(!exists);
+        setLoading(false);
+      }
+    }, 500),
+    []
+  );
+
+  useEffect(() => {
+    checkUsername(formValue);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [formValue]);
+
+  async function onSubmit(e: FormEvent) {
+    // TODO - apply error handler
+
+    e.preventDefault();
+
+    const userDoc = firestore.doc(`users/${user.uid}`);
+    const usernameDoc = firestore.doc(`usernames/${formValue}`);
+
+    const batch = firestore.batch();
+
+    batch.set(userDoc, {
+      username: formValue,
+      photoURL: user.photoURL,
+      displayName: user.displayName,
+    });
+    batch.set(usernameDoc, { uid: user.uid });
+
+    await batch.commit();
+  }
+
+  return (
+    <>
+      {!username && (
+        <section>
+          <h3>Choose Username</h3>
+          <form onSubmit={onSubmit}>
+            <input
+              name="username"
+              placeholder="username"
+              onChange={onChange}
+              value={formValue}
+            />
+            <UsernameMessage
+              username={formValue}
+              isValid={isValid}
+              loading={loading}
+            />
+            <button type="submit" className="btn-green" disabled={!isValid}>
+              Choose
+            </button>
+
+            <h3>Debug State</h3>
+            <div>
+              Username: {formValue}
+              <br />
+              Loading: {loading.toString()}
+              <br /> Username Valid: {isValid.toString()}
+            </div>
+          </form>
+        </section>
+      )}
+    </>
+  );
+}
+
+interface UsernameMessageProps {
+  username: string;
+  isValid: boolean;
+  loading: boolean;
+}
+
+function UsernameMessage({ username, isValid, loading }: UsernameMessageProps) {
+  if (loading) {
+    return <p>Checking...</p>;
+  } else if (isValid) {
+    return <p className="text-success">{username} is available!</p>;
+  } else if (username && username.length < 3) {
+    return <p className="text-danger">That username is too short!</p>;
+  } else if (username && !isValid) {
+    return <p className="text-danger">That username is taken!</p>;
+  } else {
+    return <p></p>;
+  }
 }
