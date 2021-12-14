422b737c921db3ab539470f104070d00d1d36a7a
Hugo <hugolacerdar@gmail.com>
2021-12-14 18:48:32 -0300

    Add user profile page body

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,3 +1,58 @@
-export default function UserProfilePage({}) {
-  return <main>User Profile</main>;
+import { GetServerSideProps } from "next";
+import PostFeed from "../../components/PostFeed";
+import UserProfile from "../../components/UserProfile";
+import { getUserWithUsername, postToJSON } from "../../lib/firebase";
+
+interface Post {
+  title: string;
+  slug: string;
+  uid: string;
+  username: string;
+  published: boolean;
+  content: string;
+  createdAt: number;
+  updatedAt: number;
+  heartCount: number;
 }
+interface User {
+  username: string;
+  uid: string;
+  displayName: string;
+  photoURL: string;
+}
+interface UserProfilePageProps {
+  user: User;
+  posts: Post[];
+}
+
+export default function UserProfilePage({ user, posts }: UserProfilePageProps) {
+  return (
+    <main>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
+    </main>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { username } = query;
+  const userDoc = await getUserWithUsername(username as string);
+
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .limit(5);
+
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts },
+  };
+};
