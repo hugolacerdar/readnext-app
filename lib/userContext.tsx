fa4be5b518dbc0a13a54c3d45b4066ff24aeb014
Hugo <hugolacerdar@gmail.com>
2021-12-14 15:09:28 -0300

    Update user interface

--- lib/userContext.tsx
+++ lib/userContext.tsx
@@ -3,6 +3,8 @@ import { createContext } from "react";
 interface UserContextProps {
   user: {
     photoURL?: string;
+    uid?: string;
+    displayName?: string;
   };
   username: string | undefined;
 }
