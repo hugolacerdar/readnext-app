6ee62c587b5bb191a68efd4c6c8ef2bbfc9ecaef
Hugo <hugolacerdar@gmail.com>
2021-12-14 21:47:05 -0300

    Add metatags

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,3 +1,15 @@
+import Head from "next/head";
+import Metatags from "../../components/Metatags";
+
 export default function AdminPostsPage({}) {
-  return <main>Admin Posts</main>;
+  return (
+    <main>
+      <Metatags
+        title="Admin Page"
+        description="The READNEXT admin page."
+        image=""
+      />
+      <h1>Admin Posts</h1>
+    </main>
+  );
 }
