5de5bb51a7f7ebafaa9b2061eca709dea1de6905
Hugo <hugolacerdar@gmail.com>
2021-12-17 11:09:21 -0300

    Add Image TaskEvent STATE_CHANGED export

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -22,6 +22,7 @@ export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
+export const STATE_CHANGED = firebase.storage.TaskEvent.STATE_CHANGED;
 export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp;
 
 export type DocumentReference =
