2d7d31107fb046855c27d67a7540afa746ad45f0
Hugo <hugolacerdar@gmail.com>
2021-12-21 23:58:18 -0300

    Add sign out button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,32 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useContext } from "react";
+import { auth } from "../lib/firebase";
 import { UserContext } from "../lib/userContext";
 
 export default function Navbar() {
   const { user, username } = useContext(UserContext);
 
+  const router = useRouter();
+
+  const signOut = () => {
+    auth.signOut();
+    router.reload();
+  };
   return (
     <nav className="navbar">
       <ul>
         <li>
           <Link href="/" passHref>
-            <button className="btn-logo">READNEXT</button>
+            <button className="btn-logo">READNXT</button>
           </Link>
         </li>
         {username && (
           <>
             <li className="push-left">
+              <button onClick={signOut}>Sign Out</button>
+            </li>
+            <li>
               <Link href="/admin" passHref>
                 <button className="btn-blue">Write Posts</button>
               </Link>
